98867704d1fa3e4e7cf787d0c26ff1a9156793e9
Kazu <bastian.zarate.ayala@cftmail.cl>
2024-10-12 22:40:55 -0300

    hola

---
+++ esqueleto_angular/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { RegistrateComponent } from './componentes/registrate/registrate.component';
+
+export const routes: Routes = [
+    {
+        path:"",
+        component:InicioComponent
+    },
+    {
+        path:"contactanos",
+        component:ContactoComponent,
+    },
+    {
+        path:"registro",
+        component:RegistrateComponent,
+    }
+];
